cd5280f4c897464cba30f1d5f555bccc071866ae
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-11-29 21:54:14 +0100

    Adds fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 import { ThemeProvider } from "styled-components";
+import { load } from "fathom-client";
 import type { AppProps } from "next/app";
 
 import GlobalStyle from "../styles/GlobalStyle";
 import DarkTheme from "../styles/DarkTheme";
 
+const FATHOM_SITE_ID = "TIILVVCO";
+
 function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    load(FATHOM_SITE_ID, {
+      url: "https://warbler.resumebuilder.dev/script.js",
+      includedDomains: ["bosnianswho.design", "www.bosnianswho.design"],
+    });
+  }, []);
   return (
     <>
       <ThemeProvider theme={DarkTheme}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,10 @@
 import Document, {
   Html,
-  Head,
   Main,
   NextScript,
   DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
-import constants from "../lib/constants";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -40,25 +37,6 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${constants.GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                dataLayer.push(arguments);
-              }
-              gtag("js", new Date());
-              gtag("config", "${constants.GA_TRACKING_ID}", {
-                page_path: window.location.pathname,
-              });
-              `,
-            }}
-          />
-        </Head>
         <body>
           <Main />
           <NextScript />
