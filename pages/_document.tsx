46a6833ee36f841e4f860e4cfcb335b4ca313e60
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-22 18:17:54 +0100

    Improves meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import Document, { Html, Main, NextScript, Head } from "next/document";
 
+const DESCRIPTION =
+  "A directory of inspiring Bosnians and Herzegovinians in the IT industry.";
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -9,15 +12,19 @@ export default class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <meta charSet="utf-8" />
-          <meta
-            name="og:description"
-            content="Directory of Bosnians and Herzegovinians in IT industry."
-          />
-          <meta
-            name="description"
-            content="Directory of Bosnians and Herzegovinians in IT industry."
-          />
-          <meta property="og:image" content="opengraph.png"></meta>
+          <meta property="og:title" content="Bosnians Who Design" />
+          <meta name="og:description" content={DESCRIPTION} />
+          <meta name="description" content={DESCRIPTION} />
+          <meta property="og:image" content="opengraph.png" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="https://bosnianswho.design" />
+          <meta property="og:type" content="website" />
+          <meta property="og:determiner" content="a" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:url" content="https://bosnianswho.design" />
+          <meta name="twitter:title" content="Bosnians Who Design" />
+          <meta name="twitter:description" content={DESCRIPTION} />
+          <meta name="twitter:image" content="opengraph.png" />
         </Head>
         <body>
           <ColorModeScript initialColorMode="dark" />
