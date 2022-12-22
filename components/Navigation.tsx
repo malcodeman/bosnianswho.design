cb4f5bdaec917fe53974382fc0a6ca347472706c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-22 17:53:19 +0100

    Adds change language button

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,10 +1,21 @@
-import { Text, VStack, Heading, Flex, Image } from "@chakra-ui/react";
+import { Text, VStack, Heading, Flex, Image, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { equals } from "ramda";
+import { Globe } from "react-feather";
 import { useTranslation } from "react-i18next";
 
 import NavLink from "./misc/NavLink";
 
 function Navigation() {
-  const { t } = useTranslation("common");
+  const { i18n, t } = useTranslation("common");
+  const router = useRouter();
+
+  function onChangeLang() {
+    const { pathname, asPath, query } = router;
+    const locale = equals(i18n.language, "en") ? "bs" : "en";
+    router.push({ pathname, query }, asPath, { locale });
+  }
+
   return (
     <VStack align="flex-start">
       <NavLink href="/">
@@ -25,6 +36,13 @@ function Navigation() {
       </NavLink>
       <NavLink href="/about">{t("about-this-project")}</NavLink>
       <NavLink href="/nominate">{t("nominate")}</NavLink>
+      <Button
+        size="sm"
+        leftIcon={<Globe size={16} />}
+        onClick={() => onChangeLang()}
+      >
+        {equals(i18n.language, "en") ? "Bosanski" : "English"}
+      </Button>
     </VStack>
   );
 }
