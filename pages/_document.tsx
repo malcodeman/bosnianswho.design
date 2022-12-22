5864d886b9a031cdcbfd150a7510ff62b120e988
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-22 23:03:28 +0100

    Updates meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,28 +3,82 @@ import Document, { Html, Main, NextScript, Head } from "next/document";
 
 const DESCRIPTION =
   "A directory of inspiring Bosnians and Herzegovinians in the IT industry.";
+const URL = "https://bosnianswho.design";
+const TITLE = "Bosnians Who Design";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta charSet="utf-8" />
-          <meta property="og:title" content="Bosnians Who Design" />
-          <meta name="og:description" content={DESCRIPTION} />
+          <meta name="application-name" content={TITLE} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={TITLE} />
           <meta name="description" content={DESCRIPTION} />
-          <meta property="og:image" content="opengraph.png" />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="https://bosnianswho.design" />
-          <meta property="og:type" content="website" />
-          <meta property="og:determiner" content="a" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:url" content="https://bosnianswho.design" />
-          <meta name="twitter:title" content="Bosnians Who Design" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="twitter:card" content={DESCRIPTION} />
+          <meta name="twitter:url" content={URL} />
+          <meta name="twitter:title" content={TITLE} />
           <meta name="twitter:description" content={DESCRIPTION} />
           <meta name="twitter:image" content="opengraph.png" />
+          <meta name="twitter:creator" content="@bosniansdesign" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:description" content={DESCRIPTION} />
+          <meta property="og:site_name" content={TITLE} />
+          <meta property="og:url" content={URL} />
+          <meta property="og:image" content="opengraph.png" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode="dark" />
