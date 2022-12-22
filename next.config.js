d452f54dc3bdf990ac6fb10181fb0ca56d349de6
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-22 18:39:25 +0100

    Makes app progressive

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 const { i18n } = require("./next-i18next.config");
 
-module.exports = {
+module.exports = withPWA({
   i18n,
   swcMinify: true,
-};
+});
